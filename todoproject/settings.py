477e9ca87eee1cea22a46655105dfe01ad80a703
rice-hill <heavenhayao2316@gmail.com>
2022-05-13 19:39:13 +0900

    todo 動いた

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todoapp',
+    'todo.apps.TodoConfig',
 ]
 
 MIDDLEWARE = [
